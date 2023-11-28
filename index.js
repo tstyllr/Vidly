1c9b62829df1e071e55b790cb63abd665a35f8ec
yangzhicong <770633567@qq.com>
2023-11-28 10:24:57 +0800

    A save course in db

--- index.js
+++ index.js
@@ -2,113 +2,103 @@ const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
 const Joi = require("joi");
+Joi.objectId = require('joi-objectid')(Joi)
 
 const db = config.get("db");
 mongoose.connect(
   db,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to", db);
-  }
-);
+  {}).then(res => {
+    console.log('Connected db', db);
+  }).catch(error => {
+    console.log("Failed to connect db", db);
+  })
+
+const dbCourseSchema = new mongoose.Schema({ name: String });
+const Course = mongoose.model("Course", dbCourseSchema);
+
+const validateCourse = function (course) {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+  });
+  return schema.validate(course);
+}
+
+const validateObjectId = function (id) {
+  const schema = Joi.object({
+    id: Joi.objectId(),
+  });
+  return schema.validate(id);
+}
 
 const port = process.env.PORT || config.get("port");
 const app = express();
-
 app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log("my middleware 1");
-  next();
+app.get("/api/courses", async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.send(courses);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
-app.use((req, res, next) => {
-  console.log("my middleware 2");
-  next();
-});
+app.get("/api/courses/:id", async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.id);
+    if (!course) {
+      res.status(404).send(`Course ${req.params.id} is not exist`);
+      return;
+    }
 
-app.use((req, res, next) => {
-  console.log("my middleware 3");
-  next();
-});
-
-let courses = [
-  { id: "1", name: "course1" },
-  { id: "2", name: "course2" },
-  { id: "3", name: "course3" },
-];
-
-app.get("/api/courses", (req, res) => {
-  console.log("receive request", req.params);
-  res.send(courses);
-});
-
-const idSchema = Joi.object({
-  id: Joi.string().required(),
-});
-
-app.get("/api/courses/:id", (req, res) => {
-  const { error } = idSchema.validate(req.params);
-  if (error) {
+    res.send(course);
+  } catch (error) {
     res.status(400).send(error.message);
-    return;
   }
-
-  const course = courses.find((x) => {
-    return x.id === req.params.id;
-  });
-  if (!course) {
-    res.status(404).send(`Course with id ${req.params.id} is not found!`);
-    return;
-  }
-
-  res.send(course);
 });
 
-const courseSchema = Joi.object({
-  name: Joi.string().required(),
-});
-app.post("/api/courses", (req, res) => {
-  const { error } = courseSchema.validate(req.body);
-  if (error) {
+app.post("/api/courses", async (req, res) => {
+  try {
+    const { error } = validateCourse(req.body);
+    if (error) {
+      throw error;
+    }
+
+    const course = new Course(req.body);
+    await course.save();
+    res.send(course);
+  } catch (error) {
     res.status(400).send(error.message);
-    return;
   }
-
-  const course = {
-    ...req.body,
-    id: courses.length + 1,
-  };
-  courses.push(course);
-
-  res.send(course);
 });
 
-app.put("/api/courses/:id", (req, res) => {
-  console.log(req.params, req.body);
-  const { error: error1 } = idSchema.validate(req.params);
-  if (error1) {
-    res.status(400).send(error1.message);
-    return;
+app.put("/api/courses/:id", async (req, res) => {
+  try {
+    const { error: error1 } = validateObjectId(req.params);
+    if (error1) throw error1;
+
+    const { error: error2 } = validateCourse(req.body);
+    if (error2) throw error2;
+
+    const course = await Course.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+    });
+    res.send(course);
+  } catch (error) {
+    res.status(400).send(error.message);
   }
+});
 
-  const { error: error2 } = courseSchema.validate(req.body);
-  if (error2) {
-    res.status(400).send(error2.message);
-    return;
+app.delete("/api/courses/:id", async (req, res) => {
+  try {
+    const { error } = validateObjectId(req.params);
+    if (error) throw error1;
+
+    const course = await Course.findByIdAndDelete(req.params.id);
+    res.send(course);
+  } catch (error) {
+    res.status(400).send(error.message);
   }
-
-  const courseIdx = courses.findIndex(x => x.id === req.params.id);
-  if (courseIdx === -1) {
-    res.status(404).send(`Course with id ${req.params.id} is not found!`);
-    return;
-  }
-  const course = courses[courseIdx];
-
-  let next = { ...course, ...req.body };
-  courses.splice(courseIdx, 1, next);
-
-  res.send(next);
 });
 
 app.listen(port, () => {

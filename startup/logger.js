f4c97efdf6bdcb938cc85d871ecf45bd93cd781d
yangzhicong <770633567@qq.com>
2023-12-19 09:53:10 +0800

    U 打印格式

--- startup/logger.js
+++ startup/logger.js
@@ -5,16 +5,15 @@ const db = config.get("db");
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
   // defaultMeta: { service: 'user-service' },
   transports: [
     //
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
-    new winston.transports.File({ filename: 'error.log', dirname: 'logs', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log', dirname: 'logs' }),
-    new winston.transports.MongoDB({ db, level: "error" }),
+    new winston.transports.File({ filename: 'error.log', dirname: 'logs', level: 'error', format: winston.format.combine(winston.format.timestamp(), winston.format.prettyPrint()), }),
+    new winston.transports.File({ filename: 'combined.log', dirname: 'logs', format: winston.format.combine(winston.format.timestamp(), winston.format.prettyPrint()), }),
+    new winston.transports.MongoDB({ db, level: "error", format: winston.format.combine(winston.format.timestamp(), winston.format.json()), }),
   ],
 });
 
